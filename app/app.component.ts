f7613572e923586a0db288220c3744d202bfb118
sachindeshpande <sachin.gbox@gmail.com>
2016-04-18 14:52:29 -0700

    working version now.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,5 @@
 import {Component} from 'angular2/core';
 
-public heroes = HEROES;
-
 export class Hero {
   id: number;
   name: string;
@@ -30,17 +28,3 @@ export class AppComponent {
 	name: 'Windstorm'
 	};
 }
-
-
-var HEROES: Hero[] = [
-  { "id": 11, "name": "Mr. Nice" },
-  { "id": 12, "name": "Narco" },
-  { "id": 13, "name": "Bombasto" },
-  { "id": 14, "name": "Celeritas" },
-  { "id": 15, "name": "Magneta" },
-  { "id": 16, "name": "RubberMan" },
-  { "id": 17, "name": "Dynama" },
-  { "id": 18, "name": "Dr IQ" },
-  { "id": 19, "name": "Magma" },
-  { "id": 20, "name": "Tornado" }
-];
