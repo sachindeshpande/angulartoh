523d0a81e7eb843f78ce5c8b5384c6d03848f36c
sachindeshpande <sachin.gbox@gmail.com>
2016-04-18 14:31:31 -0700

    commit app folder.

---
+++ app/app.component.ts
@@ -0,0 +1,46 @@
+import {Component} from 'angular2/core';
+
+public heroes = HEROES;
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
+@Component({
+    selector: 'my-app',
+	template:`
+	  <h1>{{title}}</h1>
+	  <h2>My Heroes</h2>
+	  <h2>{{hero.name}} details!</h2>
+	  <div><label>id: </label>{{hero.id}}</div>
+	  <div>
+		<label>name: </label>
+		<input [(ngModel)]="hero.name" placeholder="name">
+	  </div>
+	  `
+
+
+})
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+	hero: Hero = {
+	id: 1,
+	name: 'Windstorm'
+	};
+}
+
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
